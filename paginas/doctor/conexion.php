40ad12629532265af2f7990a047d451e1bd0d325
1Don2angello <joseangel0e1@gmail.com>
2023-06-05 17:03:59 -0500

    conexion a sql server

--- paginas/doctor/conexion.php
+++ paginas/doctor/conexion.php
@@ -1,5 +1,5 @@
 <?php
-     define('BD_SERVER', 'localhost');
+    /*  define('BD_SERVER', 'localhost');
      define('DB_USER','root');
      define('DB_PASS','');
      define('DB_NAME','AutocarV4');
@@ -11,5 +11,16 @@
      }
      else {
           echo '';
-     }
-?>
+     } */
+$serverName = "LAPTOP-GOI9E2B5\SQLEXPRESS";
+$database = "gestion_citas";
+$username = "admin";
+$password = "admin123456789";
+
+try {
+    $conn = new PDO("sqlsrv:Server=$serverName;Database=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a SQL Server";
+} catch (PDOException $e) {
+    echo "Error al conectar a SQL Server: " . $e->getMessage();
+}
